01434aa797f1147bf4681a9e4e3b68e3cb1cbf71
solbero <njord.solberg@gmail.com>
2022-03-06 20:38:26 +0100

    Change install script to bash

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+SOURCE_DIR=$PWD
+BIN_DIR="/usr/local/bin/"
+XSESSION_DIR="/usr/share/xsessions/"
+
+XSESSION_FILE="steam.desktop"
+SESSION_SCRIPT_FILE="steam-session"
+
+if [[ $EUID != 0 ]]; then
+    echo "You need to have root privileges to run this script."
+    echo "Please try again using 'sudo'. Exiting."
+    exit 1
+fi
+
+if [[ -z $@  || $1 == "--help" ]]; then
+    echo "Usage install.py [OPTION]"
+    echo "OPTIONS:"
+    printf "    %-4s%s\n" "-i," "--install"
+    printf "    %-4s%s\n" "-r," "--remove"
+    printf "    %-4s%s\n" "" "--help"
+
+    exit 0
+
+elif [[ $1 == "--install" || $1 == "-i" ]]; then
+
+    if [[ ! -d "$XSESSION_DIR" ]]; then
+        echo "Directory '$XSESSION_DIR' does not exist. Creating."
+    fi
+
+    cp -v "$SOURCE_DIR$SESSION_SCRIPT_FILE" "$BIN_DIR$SESSION_SCRIPT_FILE"
+    chmod -v +x "$BIN_DIR$SESSION_SCRIPT_FILE"
+    cp -v "$SOURCE_DIR$XSESSION_FILE" "$XSESSION_DIR$XSESSION_FILE"
+
+    exit 0
+
+elif [[ $1 == "--remove" || $1 == "-r" ]]; then
+
+    rm -v "$BIN_DIR$SESSION_SCRIPT_FILE"
+    rm -v "$XSESSION_DIR$XSESSION_FILE"
+
+    exit 0
+
+else
+    echo "Unrecognized arguments: '$@'"
+    echo "Try '$(basename "$0") --help' for more information"
+
+    exit 1
+
+fi
